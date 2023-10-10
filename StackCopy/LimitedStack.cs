c8d47f81879b280e5d8931a5776d4e1ea37fe952
ytnoo <ytnoo@MSI>
2023-10-11 00:53:11 +0700

    Добавьте файлы проекта.

---
+++ StackCopy/LimitedStack.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackCopy
+{
+    public class LimitedStack<T>
+    {
+        private readonly int maxSize;
+        private LinkedList<T> list;
+
+        public bool IsEmpty => list.Count == 0;
+        // Это свойство возвращает true, если стек пуст(количество элементов равно 0), и false в противном случае.
+        public int Count => list.Count;
+        // Это свойство возвращает текущее количество элементов в стеке, используя свойство Count объекта list.
+        public LimitedStack(int maxSize)
+        {
+            this.maxSize = maxSize;
+            list = new LinkedList<T>();
+        }
+      // Макс. размер стека и инициализирует поля maxSize и List
+        public LimitedStack(LimitedStack<T> other)
+        {
+            maxSize = other.maxSize;
+            list = new LinkedList<T>(other.list);
+        }
+
+        public void Push(T item) // Добавляет элемент на вершину стека и проверяет не достигнут макс. размер стека
+        {
+            if (list.Count == maxSize)
+            {
+                throw new InvalidOperationException("Stack is full");
+            }
+
+            list.AddLast(item);
+        }
+
+        public T Pop() // Метод Pop удаляет и возвращает элемент с вершины стека. Так же проверяет, является ли стек пустым
+        {
+            if (list.Count == 0)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+
+            T item = list.Last.Value;
+            list.RemoveLast();
+            return item;
+        }
+    }
+}
---
+++ StackCopy/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace StackCopy;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите размер стека:");
+        int size = int.Parse(Console.ReadLine());
+        int count = 0; // отслеживание кол-ва элементов в стеке
+        LimitedStack<int> stack = new LimitedStack<int>(size);
+        string input = null;
+        bool overflow = true; // контроль выполнения цикла
+        while (overflow)
+        {
+            if (count < size)
+            {
+                Console.Write("Введите элемент: ");
+                input = Console.ReadLine();
+                int item = int.Parse(input);
+                stack.Push(item);
+                count++;
+            }
+            LimitedStack<int> copiedStack = new LimitedStack<int>(stack);
+            if (count >= size)
+            {
+                while (!stack.IsEmpty)
+                {
+                    Console.Write(stack.Pop());
+                }
+                overflow = false;
+                Console.WriteLine("\ncopy");
+                while (!copiedStack.IsEmpty)
+                {
+                    Console.Write(copiedStack.Pop());
+                }
+            }
+        }
+    }
+}
